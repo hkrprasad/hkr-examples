ba9595390d586df7994199e0ed460d0a3ad92122
H K Ramaprasad <hkrprasad@yahoo.com>
2018-10-07 19:24:38 +0530

    Initial check-in

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -3,7 +3,8 @@ package main;
 class StartUp{
   public static void main(String[] args) {
   
-    System.out.println("Hello World\n");
+    System.out.println("Starting Main....\n");
+    
   }
 
 }
